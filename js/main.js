5240e1403183280d752c2398dac35f819973d292
td0 <tadho@me.com>
2016-08-19 14:17:42 +0700

    Parser API is ready to use

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 {
 	$inputButton = document.querySelector("#inputButton");
   $fileInput = document.querySelector("#fileInput");
-
+  $output = document.querySelector("#outputSection");
+  var fileName = "";
+  var s = new Parser();
 
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
 	  // Great success! All the File APIs are supported.
@@ -14,7 +16,7 @@
 	}
 
 	FileReaderJS.setupInput($fileInput, {
-		readAsDefault: "DataURL",	
+		readAsDefault: "BinaryString",	
   	on:{
   		beforestart: (file) => {
   			if ( file.name.split(".")[1] != 'sav'){
@@ -26,8 +28,11 @@
   			}
   		},
   		load: (e, file) => {
-  			console.log(parseTitle(file.extra.nameNoExtension));
-  			
+  			// fileName = file.extra.nameNoExtension;
+  			// console.log(parseTitle(fileName));
+  			// console.log (e.currentTarget.result);
+  			s.data = e.currentTarget.result;
+  			renderData(file);
   		},
   		loadend: (e,file) => {
   			console.log("loadend");
@@ -36,12 +41,18 @@
 	});
 
 	function parseTitle(fn){
+		fn = fn || fileName;
 		fn = fn.toLowerCase().replace(/ /g,"");
 		let titles = ['firered','leafgreen','emerald','ruby','sapphire','fr','lg'];
 		let i = 0;
 		for(;i<titles.length;i++){
 			if (fn.search(titles[i])!=-1) break;
 		}
-		return i<7?titles[i%4]:"unidentified";
+		return i<7?titles[i%4]:false;
+	}
+
+	function renderData(f){
+		let gameVersion = parseTitle(f.extra.nameNoExtension) || "unidentified";
+		$output.innerHTML = `<img src='assets/logo/${gameVersion}.png' height="70">`;
 	}
 }
--- js/parser.js
+++ js/parser.js
@@ -1,24 +1,89 @@
 class Parser{
-	constructor(sav, region="en"){
-		this.sav = sav;
-		this.charMap = region;
+	constructor(data, region="en"){
+		this.data = data;
+		this._charMap = region=="en"?en_ch:jp_ch;
+
+
+	}
+	_chekSum(section=0){
+		let sum = 0;
+		for(let i = 0; i<sumBytes[section];){
+			break;
+		}
+	}
+	_getInt([ofst,size],str=false){ //set str to true to receive stringified-hex.
+		let charSet = "";
+		for(let i=ofst+size-1;i>=ofst;i--){ //this loop runs on decrement, little-endian bruh!
+			let temp = this.data.charCodeAt(i).toString(16);
+			if(temp.length<2) temp = "0"+temp;
+			charSet+=temp;
+		}
+		return str?charSet:parseInt("0x"+charSet);
 	}
 	_getChar(i){
-		return this.charMap[i];
+		let hex = this.data.charCodeAt(i).toString(16);
+		return hex.length == 2?hex:"0"+hex ;
 	}
-	_getString(ofst,length){
+	_getString([ofst,size],debug=false){
 		let str = "";
-		for(i=ofst;i<length+ofst;i++){
-			if(this.sav.charCodeAt(i) == 0xFF) break;
-			str += _getChar(this.sav.charCodeAt(i));
+		for(let i=ofst; i<ofst+size; i++){	 //the string however, runs on increment		
+			if(this.data.charCodeAt(i)==0xFF || debug) break;
+			str+=this._charMap[this.data.charCodeAt(i)];
 		}
 		return str;
 	}
 
+	getTrainerName(){
+		return this._getString(dataMap.trainerName);
+	}
+	getTrainerGender(){
+		return this.data.charCodeAt(0x2008)==0?"Male":"Female";
+	}
+	getTrainerID(){
+		return [this._getInt(dataMap.trainerID[0]),this._getInt(dataMap.trainerID[1])];
+	}
+
+
+	// Tis' debug 'em
+	encdStr(s){
+		let res = "";
+		for(let i=0; i<s.length; i++){
+			res += en_ch.indexOf(s[i]).toString(16).toUpperCase();
+		}
+		return res;
+	}
+	dcdStr(s){
+		s=s.replace(/ /g,'');
+		let [res,f,temp] = ["",false,"0x"];
+		for(let i =0; i<s.length;i++){
+			temp += s[i];
+			if(f){	
+				res+=en_ch[parseInt(temp)];
+				temp = "0x";
+			}
+			f=!f;
+		}
+		return res;
+	}
 }
 
+
+var sumBytes = [3884,3968,3968,3968,3848,3968,3968,3968,3968,3968,3968,3968,2000];
+//Here be dragons
+var dataMap = {
+	trainerName : [0x2000,7],
+	trainerGender : [0x2008,1],
+	trainerID : [[0x200A,2],[0x200C,2]],
+	timePlayed : [0x200E,5],
+	gameCode : [0x20AC,4],
+	securityCode : {
+		frlg : [0x2AF8,4],
+		e : [0x20AC,4]
+	}
+};
+
 var en_ch = 
-	["&nbsp;", "À", "Á", "Â", "Ç", "È", "É", "Ê", "Ë", "Ì", "こ", "Î", "Ï", "Ò", "Ó", "Ô",
+	[" ", "À", "Á", "Â", "Ç", "È", "É", "Ê", "Ë", "Ì", "こ", "Î", "Ï", "Ò", "Ó", "Ô",
 	"Œ", "Ù", "Ú", "Û", "Ñ", "ß", "à", "á", "ね", "ç", "è", "é", "ê", "ë", "ì", "ま",
 	"î", "ï", "ò", "ó", "ô", "œ", "ù", "ú", "û", "ñ", "º", "ª", "˘", "&amp;", "+", "あ",
 	"ぃ", "ぅ", "ぇ", "ぉ", "<small>Lv</small>", "=", "ょ", "が", "ぎ", "ぐ", "げ", "ご", "ざ", "じ", "ず", "ぜ",
@@ -36,7 +101,7 @@ var en_ch =
 	":", "Ä", "Ö", "Ü", "ä", "ö", "ü", "⬆", "⬇", "⬅", "[1]","[2]","[3]","[4]","<br />", "[0]"];
 
 var jp_ch =
-	["&nbsp;", "あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ",
+	[" ", "あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ",
 	"た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま",
 	"み", "む", "め", "も", "や", "ゆ", "よ", "ら", "り", "る", "れ", "ろ", "わ", "を", "ん", "ぁ",
 	"ぃ", "ぅ", "ぇ", "ぉ", "ゃ", "ゅ", "ょ", "が", "ぎ", "ぐ", "げ", "ご", "ざ", "じ", "ず", "ぜ",
